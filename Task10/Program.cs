3b4af161ad6b5c1143353fd4c9cca761a8066664
Denis Ternovoi <denis.ternovoi@mail.ru>
2023-01-28 11:51:55 +0300

    Added Task13

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,13 @@
-﻿Console.Write("Как Вас зовут? ");
-Console.ReadLine();
+﻿Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int NewNumber(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = firstDigit % 10;
+    return secondDigit;
+}
+
+int newNumber = NewNumber(number);
+Console.WriteLine($"Вторая цифра → {newNumber}");
---
+++ Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int isThirdDigit(int num)
+{
+    return num / 10;
+}
+
+int result = isThirdDigit(number);
+if (result > 9) Console.WriteLine(number % 10);
+else Console.WriteLine("третьей цифры нет");
+
