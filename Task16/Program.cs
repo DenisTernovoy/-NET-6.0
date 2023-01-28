09cb2701bfcc91a2b7b4b780b0cc1afc02f8646a
Denis Ternovoi <denis.ternovoi@mail.ru>
2023-01-28 11:28:07 +0300

    Added Task14

---
+++ Task16/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+string isSquare(int num1, int num2)
+{
+    if (num1 * num1 == num2) return "Да";
+    else return "Нет";
+}
+
+string result = isSquare(number1, number2);
+Console.WriteLine(result);
