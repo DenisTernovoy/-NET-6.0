578d031b749ffe7f05e268b019590577cbf8bf9b
Denis Ternovoi <denis.ternovoi@mail.ru>
2023-01-28 21:11:18 +0300

    Added Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите целое положительное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int num) 
+{
+    int sum = 0;
+    for (int i = 0; i <= num; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
