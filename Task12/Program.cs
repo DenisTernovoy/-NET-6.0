294e2bacc0292885ec68ac788ebc666de369d9c9
Denis Ternovoi <denis.ternovoi@mail.ru>
2023-01-28 11:22:41 +0300

    Added Task16

---
+++ Task12/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int Division(int num1, int num2)
+{
+    return num1 % num2;
+}
+
+int result = Division(number1, number2);
+if (result == 0) Console.WriteLine("Кратно");
+else Console.WriteLine($"не кратно, остаток {result}");
