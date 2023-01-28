838af52293463644af0655be29bf8c55027597f8
Denis Ternovoi <denis.ternovoi@mail.ru>
2023-01-28 11:04:27 +0300

    Added Task12

---
+++ Task11/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Входящее число → {number}");
+
+int NewNumber(int num)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    string newstring = Convert.ToString(firstDigit) + Convert.ToString(thirdDigit);
+    return Convert.ToInt32(newstring);
+}
+
+int newNumber = NewNumber(number);
+Console.WriteLine($"Новое число → {newNumber}");
