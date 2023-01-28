690a5dc622519ec4550c6f1191962a3c6e67ab22
Denis Ternovoi <denis.ternovoi@mail.ru>
2023-01-28 12:53:46 +0300

    Added Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите номер четверти:");
+int qn = Convert.ToInt32(Console.ReadLine());
+
+string Quarter(int q)
+{
+    if (q == 1) return ("x > 0 and y > 0");
+    if (q == 2) return ("x < 0 and y > 0");
+    if (q == 3) return ("x < 0 and y < 0");
+    if (q == 4) return ("x > 0 and y < 0");
+    return "Введите корректные данные!";
+}
+
+string result = Quarter(qn);
+Console.WriteLine(result);
