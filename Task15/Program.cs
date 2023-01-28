94d0ad2cd9a665d724e50151491937a5ec1482e2
Denis Ternovoi <denis.ternovoi@mail.ru>
2023-01-28 11:56:36 +0300

    Added Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите номер дня недели:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool isWeekend(int num)
+{
+    return num == 6 | num == 7;
+}
+
+bool result = isWeekend(number);
+if (result) Console.WriteLine("да");
+else Console.WriteLine("нет");
