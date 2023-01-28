fb39e30e398cb0b0569e91a6be32528141d99ef4
Denis Ternovoi <denis.ternovoi@mail.ru>
2023-01-28 21:59:23 +0300

    Added Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+int result = SumDigits(number);
+Console.WriteLine($"{number} -> {result}");
