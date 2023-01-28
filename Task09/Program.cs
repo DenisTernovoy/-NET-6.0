97d7cdf803b9c94ce877eeb3e16e3f742a411ae8
Denis Ternovoi <denis.ternovoi@mail.ru>
2023-01-28 10:14:55 +0300

    Added Task09

---
+++ Task09/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10-99 → {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (firstDigit > secondDigit)
+    Console.WriteLine($"НАибольшая цифра числа → {firstDigit}");
+else
+    Console.WriteLine($"НАибольшая цифра числа → {secondDigit}");
+
+int result = firstDigit > secondDigit ? firstDigit: secondDigit;
+Console.WriteLine($"НАибольшая цифра числа → {result}");
