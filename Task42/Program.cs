de2b893808bf9c8a446116bae711ef5cb938cf89
Denis Ternovoi <denis.ternovoi@mail.ru>
2023-02-13 22:00:40 +0300

    Added Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+void Binary(int num)
+{
+    string result = string.Empty;
+    
+    while (num / 2 != 0) {
+        result += Convert.ToString(num % 2);
+        num /= 2;
+    }
+    result += Convert.ToString(num);
+    
+    string newResult = string.Empty;
+    for (int i = 0; i < result.Length; i++)
+    {
+        newResult += result[result.Length - 1 - i];
+    }
+    Console.WriteLine(newResult);
+}
+
+Binary(125);
