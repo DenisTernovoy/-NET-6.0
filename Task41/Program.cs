3d6e6be357852dead3caced000541d994f7b5170
Denis Ternovoi <denis.ternovoi@mail.ru>
2023-02-13 22:40:45 +0300

    Added Task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// -1, -7, 567, 89, 223-> 3
+
+
+Console.Write("Введите количество вводимых чисел: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArray(int num)
+{
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int CountPossitive(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count += 1;
+    }
+    return count;
+}
+
+int[] array = CreateArray(number);
+PrintArray(array);
+Console.WriteLine();
+int result = CountPossitive(array);
+Console.WriteLine($"Количество чисел больше 0 -> {result}");
