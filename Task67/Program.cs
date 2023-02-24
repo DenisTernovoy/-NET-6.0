3d0dfd11c9ec92a8c5547c4521a165600fbe1bfc
Denis Ternovoi <denis.ternovoi@mail.ru>
2023-02-24 13:31:52 +0300

    Added Task67.1

---
+++ Task67/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+void SumNumbers(int num)
+{
+    if (num == 0) return;
+    int ld = num % 10;
+    sum += ld;
+    num /= 10;
+    SumNumbers(num);
+}
+
+SumNumbers(number);
+Console.WriteLine($"Сумма цифр в числе {number} -> {sum}");
